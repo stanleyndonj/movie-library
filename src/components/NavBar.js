f2b308726b5bc6153b6ef5cabfed21e434feab51
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-13 14:48:28 +0300

    almost complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,21 @@
+// NavBar.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import "./Navbar.css";
+import './Navbar.css';
 
 const NavBar = () => {
   return (
     <div className="mainnav">
       <div>
-        <input className='search' placeholder='Search Movies' />
-      </div>
-      <div>
         <nav className="navbar">
           <NavLink to="/">Home</NavLink>
-          <NavLink to="/library">Movies</NavLink> {/* Updated the link */}
+          <NavLink to="/library">Library</NavLink> {/* Updated this NavLink */}
           <NavLink to="#">Series</NavLink>
           <NavLink to="#">Episodes</NavLink>
         </nav>
       </div>
       <div>
-        <button className='addbutton'>
-          <NavLink to="/add-movie" style={{ color: 'white' }}>Add Movie</NavLink>
-        </button>
+        <button className='addbutton'>Add Movie</button>
       </div>
     </div>
   );
