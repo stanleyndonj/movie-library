9f728584e67084f2beeb988a45acaa2774a8853e
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-09-13 14:49:26 +0300

    almost there trying to fix a few errors

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import React from 'react';
+// App.js
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Updated imports
 import MovieList from './components/MovieList';
 import AddMovieForm from './components/AddMovieForm';
 import MovieDetail from './components/MovieDetail';
 import NavBar from './components/NavBar';
 import NotFound from './components/NotFound';
-import Library from './components/Library';
-
+import Library from './components/Library'; // Import the Library component
 
 function App() {
+  // State to manage the library movies
+  const [library, setLibrary] = useState([]);
+  const [theme, setTheme] = useState('light');
+
+  // Function to add movie to the library
+  const handleAddToLibrary = (movie) => {
+    setLibrary((prevLibrary) => [...prevLibrary, movie]);
+  };
+
+  // Function to toggle theme
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <Router>
-      <div className="App">
+      <div className={`App ${theme}`}>
         <NavBar />
-        {/* Use Routes instead of Switch and update Route components */}
+        <button onClick={toggleTheme}>
+          Toggle Theme
+        </button>
         <Routes>
-          <Route path="/" element={<MovieList />} />
-          <Route path="/library" element={<Library />} />
+          <Route path="/" element={<MovieList handleAddToLibrary={handleAddToLibrary} library={library} />} />
           <Route path="/add-movie" element={<AddMovieForm />} />
+          <Route path="/library" element={<Library library={library} />} /> {/* Library route */}
           <Route path="/movies/:id" element={<MovieDetail />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
@@ -26,4 +42,4 @@ function App() {
   );
 }
 
-export default App; // Don't forget to export your App component
+export default App;
